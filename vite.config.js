9ce36727d6bc9ced59c57b58831ab058a3c85ca1
darylcitu <daryl.butar@cit.edu>
2025-02-03 00:01:38 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // This ensures the build output is in the 'dist' folder
+  },
 })
