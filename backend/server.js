cc5e2718ac6b36f67c1faedb10a41ca4a755f96d
darylcitu <daryl.butar@cit.edu>
2025-02-02 23:30:46 +0800

    .

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+// backend/server.js
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js backend!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
