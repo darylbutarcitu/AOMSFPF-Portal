6a8dc4ee87b3498193d9589a58d8c2aef1740e7f
darylcitu <daryl.butar@cit.edu>
2025-02-03 23:43:27 +0800

    Assets and CSS

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,23 +22,24 @@ function Home() {
     <>
       <div>
         <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="/mascot.svg" className="logo" alt="Sisiw Logo" />
         </a>
         <a href="https://www.youtube.com/watch?v=WzKJ1Ks3B8Y" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="/cituseal.svg" className="logo citu" alt="CIT-U logo" />
         </a>
       </div>
       <h1>Automated Odor Mitigation System for Poultry Farms</h1>
       <div className="card">
-        <button onClick={sendSignalToESP32}>
+        <button onClick={sendSignalToESP32} className="button">
           Ping ESP32
         </button>
         <p>
-          Mute your device don't say I did not warn you bhie
+          <br></br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
       </div>
       <p className="read-the-docs">
-        PLEASE LANG ROLD PAPASARA MI ANI
+        Test: SLAYYY
       </p>
     </>
   )
